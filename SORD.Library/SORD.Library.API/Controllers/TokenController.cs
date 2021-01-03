e9baf04da53900d35d31d2f330b48d7a97483f01
Norman-Noe <52176938+Norman-Noe@users.noreply.github.com>
2021-01-03 14:04:15 -0600

    fdfadf

---
+++ SORD.Library/SORD.Library.API/Controllers/TokenController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SORD.Library.API.Controllers
+{
+    public class TokenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
